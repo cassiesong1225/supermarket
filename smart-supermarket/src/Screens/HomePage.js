fd161eb81b1385a10532d65b56f9f21d40b805ea
cassiesong1225 <cassiesong1225@hotmail.com>
2024-07-08 18:16:06 -0700

    set up prototype

---
+++ smart-supermarket/src/Screens/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../Styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <nav>
+          <div className="nav-links">
+            <button className="nav-link">Home</button>
+            <button className="nav-link">Signin</button>
+          </div>
+        </nav>
+      </header>
+      <main className="main-content">
+        <div className="centered-container">
+          <div className="top-content">
+            <h1>Smart Supermarket Recommender System</h1>
+            <button className="register-btn">Register</button>
+          </div>
+        </div>
+        <div className="bottom-content"> 
+          <div className="features">
+            <div className="feature">
+              <h3>Face Recognition <br /> Emotion Detection</h3>
+            </div>
+            <div className="feature">
+              <h3>User Preference <br />Product Feature Analysis</h3>
+            </div>
+            <div className="feature">
+              <h3>Give <br />Recommendations</h3>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
