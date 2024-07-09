f98c1599fe3264f34a030324644e497804ff1a17
cassiesong1225 <cassiesong1225@hotmail.com>
2024-07-09 16:21:18 -0700

    modify registerpage to show signin and register page

--- smart-supermarket/src/App.js
+++ smart-supermarket/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Screens/HomePage';
-import RegisterPage from './Screens/RegisterPage';
+import AuthPage from './Screens/AuthPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register" element={<AuthPage />} />
+        <Route path="/signin" element={<AuthPage />} />
       </Routes>
     </Router>
   );
