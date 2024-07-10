574a3cf812ed742bf2693ae5dfbcec6a28539cbf
cassiesong1225 <cassiesong1225@hotmail.com>
2024-07-10 15:27:22 -0700

    combine two version

--- smart-supermarket/src/App.js
+++ smart-supermarket/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Screens/HomePage';
-import AuthPage from './Screens/AuthPage';
 import FaceRecognitionPage from './Screens/FaceRecognitionPage';
 
 function App() {
@@ -9,8 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<AuthPage />} />
-        <Route path="/signin" element={<AuthPage />} />
+
         <Route path="/face-recognition" element={<FaceRecognitionPage />} />
       </Routes>
     </Router>
