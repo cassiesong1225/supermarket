98548cf61fe7d3639e37d2117faff6be0e4177c0
lashinamy@gmail.com <yangyang@yangyangdeMacBook-Air.local>
2024-07-16 15:08:55 -0700

    recommend items for new users

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -3,6 +3,9 @@ import joblib
 import numpy as np
 import pandas as pd
 from datetime import datetime
+from sklearn.metrics.pairwise import cosine_similarity
+import tensorflow as tf
+
 
 
 
@@ -15,6 +18,9 @@ CF_model = joblib.load('trained_model/cf_model.pkl')
 user_product_matrix = joblib.load('trained_model/user_product_matrix.pkl')
 #product embeddings from content-based model
 product_embeddings = joblib.load('trained_model/product_embeddings.pkl')
+#user embeddings from content-based model
+user_embeddings = joblib.load('trained_model/user_embeddings.pkl')
+
 emotion_dict = {
         "happy": "positive",
         "angry": "negative",
@@ -24,11 +30,13 @@ emotion_dict = {
         "suprise": "unclassified",
         "neutral": "unclassified"
     }
+
 products_df = pd.read_csv('capstone-dataset/products.csv')
 aisles_df = pd.read_csv('capstone-dataset/aisles.csv')
 departments_df = pd.read_csv('capstone-dataset/departments.csv')
 products_with_expiration_df = pd.read_csv('capstone-dataset/products_with_expiration.csv')
-
+user_features_df =  pd.read_csv('capstone-dataset/user_features_headers.csv')
+purchase_count_train_df =  pd.read_csv('capstone-dataset/purchase_count_train_df.csv')
 
 products_df = pd.merge(products_df, aisles_df, on="aisle_id")
 products_df = pd.merge(products_df, departments_df, on="department_id")
@@ -90,26 +98,49 @@ def recommended_close_to_expiration_items(recommended_items, N=3):
     return final_union_df.head(N)
 
 
-
 def get_products_df(product_ids):
     return products_df[products_df["product_id"].isin(product_ids)]
-    
+
+
+def get_initial_recommendations_for_new_users(aisle_ids, N):
+    aisles = list(map(int, aisle_ids.split(",")))
+    purchase_count_df_with_aisles = pd.merge(purchase_count_train_df, products_df[['product_id', 'aisle_id']], on='product_id', how='left')
+    filted_df = purchase_count_df_with_aisles[purchase_count_df_with_aisles["aisle_id"].isin(aisles)]
+    topN = N // len(aisles)
+    topN = 1 if topN == 0 else topN
+    recommendations = []
+    for aisle_id in aisles:
+        items_in_aisle = filted_df[filted_df['aisle_id'] == aisle_id]
+        items_in_aisle = items_in_aisle.groupby('product_id')['purchase_count'].sum().reset_index(name='purchase_count')
+        items_in_aisle = items_in_aisle.sort_values(by='purchase_count', ascending=False)
+        top_items_in_aisle = items_in_aisle.head(topN)["product_id"].values
+        recommendations.extend(top_items_in_aisle)
+    return recommendations
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    user_id = request.args.get('userId', type=int)
+    user_id = request.args.get('userId')
     current_mood = request.args.get('mood')
     N = request.args.get('N', 10, type=int)
-
-    #generate initial recommendations
-    initial_recommendations, scores = CF_model.recommend(user_id, user_product_matrix[user_id], N=N)
+    aisle_ids = request.args.get('interested_aisles')
+    if current_mood is None:
+        return jsonify({"error": "Mood parameter is missing"}), 400
+    if user_id is None and aisle_ids is None:
+        return jsonify({"error": "user_id and interested_aisles parameters are missing"}), 400
+    
+    initial_recommendations = None
+    if user_id is None:
+        initial_recommendations = get_initial_recommendations_for_new_users(aisle_ids, N)
+    else:
+        #generate initial recommendations
+        initial_recommendations, scores = CF_model.recommend(user_id, user_product_matrix[user_id], N=N)
     #get current emotion related product list
     current_emotion_related_pros = products_mood_df[products_mood_df["mood"]==emotion_dict[current_mood]]
     #get the intersection of initial recommendations and current emotion related products
     cur_mood_relate_recommendations_df = recommended_current_emotion_related_items(initial_recommendations, current_emotion_related_pros)
-    
+    #get the intersection of initial recommendations and close-to-expiration products
     close_to_exp_recommendations_df = recommended_close_to_expiration_items(initial_recommendations)
-
-
 
     initial_recommendations_df = get_products_df(initial_recommendations)
     #format the recommendations
@@ -125,6 +156,8 @@ def predict():
     return jsonify({'initial_recommendations': initial_result_json, 
                     "mood_related_recommendations": mood_result_json,
                     "close_to_exp_recommendations": close_to_exp_result_json})
+
+
 
 if __name__ == '__main__':
     app.run(port=5525, debug=True)
